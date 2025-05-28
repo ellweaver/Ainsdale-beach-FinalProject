ccb8e48d82c382ff4957dc85b1b7d9ee6577201e
Seb <seb.allen@protonmail.com>
2025-05-28 15:33:10 +0100

    include ingestion_bucket_name

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -3,6 +3,11 @@ variable "python_bucket" {
   default = "ainsdale-python-bucket"
 }
 
+variable "ingestion_bucket_name" {
+  type    = string
+  default = "ainsdale-ingestion-bucket"
+}
+
 variable "account_arn" {
   type    = string
   default = "arn:aws:iam::048204777974:user/Ainsdale_beach"
