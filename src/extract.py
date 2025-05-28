4327002bfa445393f795680ab59e30aae0b57411
jenia-solionii <jenia.solionii@gmail.com>
2025-05-28 16:10:46 +0100

    write extract.py, upto creating all parquet files

--- src/extract.py
+++ src/extract.py
@@ -1,12 +1,72 @@
 import logging
 import boto3
+from utils.db_utils import connect_to_db, close_db_connection
+import pandas as pd
+import pyarrow as pa
+import pyarrow.parquet as pq
+import shutil 
+from datetime import datetime
 
-logger=logging.getLogger(__name__)
+client = 'b'
+
+logger = logging.getLogger(__name__)
+
+# with ZipFile('spam.zip', 'w') as myzip:
+#     myzip.write('eggs.txt')
+
 
 
 def extract_data(client):
     
-    logger.info("wooo")
-    logger.info("hoo")
-    #logger.warning("ouch")
-    return "hoo"
+    table_name_list = ["counterparty",
+                        "currency",
+                        "department",
+                        "design",
+                        "staff",
+                        "sales_order",
+                        "address",
+                        "payment",
+                        "purchase_order",
+                        "payment_type",
+                        "transaction"]
+    
+    conn = connect_to_db()
+    time_now = datetime.now()
+
+    def transformer_func(table_name_list):
+        for table in table_name_list:
+            df = pd.read_sql("SELECT * FROM " + table, conn)
+            pyarrow_table = pa.Table.from_pandas(df)
+            pq.write_table(pyarrow_table, f'data/{table}{time_now}.parquet')
+
+
+    transformer_func(table_name_list) #creates each parquet file in data directory
+    shutil.make_archive(f'data/{time_now}parquet', 'zip', r'data') # zips data directory
+
+
+extract_data(client)
+
+
+
+
+    # try:
+    #     transformer_func(*table_name_list)
+        
+    # except:
+
+
+    # finally:
+    #     close_db_connection(conn)
+    
+    # return 
+
+
+
+# get info from db
+# reformat db data 
+# to parquet 
+# file upload using util 
+# return
+
+
+
