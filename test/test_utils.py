2e619f4bc3ba9f8c7d5392f12bab8f25c44ebc8c
ellweaver <elliotweaver247@gmail.com>
2025-05-28 12:29:21 +0100

    complete download_file

--- test/test_utils.py
+++ test/test_utils.py
@@ -1,10 +1,10 @@
 import pytest
 from moto import mock_aws
 from utils.utils import upload_file, download_file
+import io
 
 
 class TestUploadFile:
-    
     @pytest.mark.it('upload file returns success on a Success')
     def test_upload_success(self, test_s3, test_bucket):
         file_name="Test_file.txt"
@@ -27,4 +27,27 @@ class TestUploadFile:
         assert response["status"] == "Failed"
         assert response["code"]=="NoSuchBucket"
 
+class TestDownloadFile:
+    @pytest.mark.it('Download file returns Success on a Successful download')
+    def test_download_success(self,test_s3,test_bucket):
+        file_name="Test_file.txt"
+        bucket="test_bucket"
+        key = file_name
+        upload_file(test_s3,file_name,bucket,key)
+        response = download_file(test_s3, bucket,key)
+        assert isinstance(response, dict)
+        assert response["code"] == 200
+        assert response["status"] =="Success"
+        fileobj=io.BytesIO(response['body'].read())
+        assert b'Test_file.txt'==fileobj.read()
 
+
+    @pytest.mark.it('Download file returns Failure on failed download')
+    def test_snake_case_name(self,test_s3,test_bucket):
+        file_name="test_file.txt"
+        bucket="test_bucket"
+        key=file_name
+        response = download_file(test_s3,bucket,key)
+        assert isinstance(response, dict)
+        assert response["status"] == "Failed"
+        assert response["code"]=="NoSuchKey"
