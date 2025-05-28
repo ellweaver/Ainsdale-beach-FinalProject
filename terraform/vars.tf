3ff79709265adbbb3f8d18aecf363743ea63b63c
Seb <seb.allen@protonmail.com>
2025-05-28 15:27:46 +0100

    create account arn variable

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -1,4 +1,9 @@
-variable "python_bucket"{
-    type=string
-    default = "ainsdale-python-bucket"
+variable "python_bucket" {
+  type    = string
+  default = "ainsdale-python-bucket"
+}
+
+variable "account_arn" {
+  type    = string
+  default = "arn:aws:iam::048204777974:user/Ainsdale_beach"
 }
