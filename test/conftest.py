198011b68e2c48ef3b66b5020001723b64d62569
ellweaver <elliotweaver247@gmail.com>
2025-05-28 10:34:58 +0100

    update docstring on test_bucket func

--- test/conftest.py
+++ test/conftest.py
@@ -21,6 +21,7 @@ def test_s3():
 
 @pytest.fixture(scope='class')
 def test_bucket(test_s3):
+    """Creates mock_bucket for client"""
     test_s3.create_bucket(Bucket="test_bucket",CreateBucketConfiguration={'LocationConstraint': "eu-west-2",
         'Location': {
             'Type': 'AvailabilityZone',
