6b13b3db7b89f8e660cb01015734686ca5fdb1dc
didrudals112 <didruals112@KYPC.localdomain>
2025-05-29 10:57:10 +0100

    completed set up alarm for the errors for ainsdale beach lambda

---
+++ terraform/cloudwatch.tf
@@ -0,0 +1,24 @@
+resource "aws_cloudwatch_metric_alarm" "ainsdale_beach_alarm" {
+  alarm_name          = "AinsdaleBeachLambdaAlarm"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = 1
+  metric_name         = "Errors"
+  namespace           = "AWS/Lambda"
+  period              = 300  # 5 minutes
+  statistic           = "Minimum"
+  threshold           = 1   # Set your threshold value
+
+  dimensions = {
+    "extract_lambda" = {
+      FunctionName = aws_lambda_function.extract_lambda.function_name
+    }
+  }
+
+  alarm_description = "Alarm when error occurs in the ainsdale beach lambda"
+
+  actions_enabled = true
+
+  alarm_actions = ["arn:aws:sns:eu-west-2:aws_sns_topic.sns_extract"]
+
+  ok_actions = ["arn:aws:sns:your_aws_region:aws_sns_topic.sns_extract"]
+}
