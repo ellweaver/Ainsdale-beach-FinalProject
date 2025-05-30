2a3dca6123782a95786394635b272fa97f0abb0d
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 <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>
2025-05-30 17:11:25 +0100

    storing chanages for weekend

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -20,12 +20,15 @@ data "archive_file" "transform_py" {
   output_path      = "${path.module}/../terraform/data/transform.zip"
 }
 
-data "archive_file" "utils_py" {
+data "archive_file" "python_layer" {
   type             = "zip"
   output_file_mode = "0666"
-  source_file      = "${path.module}/../utils/utils.py"
-  output_path      = "${path.module}/../terraform/data/utils.zip"
+  source_dir      = "${path.module}/data/lambda_layer"
+  output_path      = "${path.module}/../terraform/data/lambda_layer.zip"
 }
+
+
+
 
 resource "aws_s3_object" "extract_file_upload" {
   bucket = aws_s3_bucket.python_bucket.bucket
@@ -50,23 +53,37 @@ resource "aws_s3_object" "transform_file_upload" {
   
 }
 
-resource "aws_s3_object" "utils_file_upload" {
+resource "aws_s3_object" "python_layer_upload" {
   bucket = aws_s3_bucket.python_bucket.bucket
-  key    = "utils_func"
-  source = data.archive_file.utils_py.output_path
-  etag = filemd5( data.archive_file.utils_py.output_path)
+  key    = "python"
+  source = data.archive_file.python_layer.output_path
+  etag = filemd5( data.archive_file.python_layer.output_path)
 
 }
 
+
+
+resource "aws_lambda_layer_version" "python_layer" {
+  s3_bucket = aws_s3_bucket.python_bucket.bucket
+  s3_key = aws_s3_object.python_layer_upload.key
+  layer_name = "python"
+
+}
+
+
+
 resource "aws_lambda_function" "extract_lambda" {
   function_name = "extract_lambda_function"
-  runtime = "python3.11"
+  runtime = "python3.13"
   role = aws_iam_role.extract_lambda_role.arn
   handler = "extract.lambda_handler"
 
   s3_bucket = aws_s3_bucket.python_bucket.bucket
   s3_key = aws_s3_object.extract_file_upload.key
-  
+  layers = [
+   aws_lambda_layer_version.python_layer.arn,
+   #"arn:aws:lambda:eu-west-2:336392948345:layer:AWSSDKPandas-Python313:2",
+   ]
 
   timeout = 60
   publish = true
