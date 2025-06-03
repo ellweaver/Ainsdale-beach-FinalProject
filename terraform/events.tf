016b5535b7dec15ba775f68a89030da9523a99e6
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 <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>
2025-06-03 16:19:12 +0100

    completed step function

--- terraform/events.tf
+++ terraform/events.tf
@@ -1,3 +1,4 @@
+/*
 resource "aws_cloudwatch_event_rule" "extract_lambda_schedule" {
   name                = "run-extract-lambda-func-every-10-mins"
   schedule_expression = "rate(10 minutes)"
@@ -18,4 +19,16 @@ resource "aws_lambda_permission" "extract_lambda_scheduler_permission" {
   source_arn    = aws_cloudwatch_event_rule.extract_lambda_schedule.arn
 
 }
+*/
+resource "aws_cloudwatch_event_rule" "ainsdale_state_machine_schedule" {
+  name                = "run-step-func-every-10-mins"
+  schedule_expression = "rate(10 minutes)"
+  description         = "Scheduler for running the extract lamdba function every ten minutes"
+}
 
+resource "aws_cloudwatch_event_target" "ainsdale_etl_stepfunction" {
+  rule      = aws_cloudwatch_event_rule.ainsdale_state_machine_schedule.name
+  target_id = "ainsdale_etl_stepfunction"
+  arn       = aws_sfn_state_machine.ainsdale_beach_etl_state_machine.arn
+  role_arn = aws_iam_role.eventbridge_role.arn
+}
